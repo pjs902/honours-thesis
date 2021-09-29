6dd9457b9e9e9fd3ca2f8c7f07e2468d98280b81
Peter Smith <smith.peter.902@gmail.com>
2021-09-29 12:33:58 -0300

    move code to package

---
+++ src/binaryshift/binaryshift.py
@@ -0,0 +1,183 @@
+import numpy as np
+
+
+class BinaryShift:
+    def __init__(self, mj, Mj, MF):
+        """
+        Initialize an instance of `BinaryShift` with the provided mass bins and MF, (TODO)
+        the IFMR will (eventually) be used to label the object type of each bin.
+        """
+
+        if len(mj) != len(Mj):
+            raise ValueError("mj and Mj must have the same length.")
+
+        self.mj = mj
+        self.Mj = Mj
+        self.mf = MF
+
+        # IFMR stuff
+        self.nms = np.sum(np.array(self.mf.Ns) > self.mf.Nmin) - 1
+        self.mWD_max = self.mf.IFMR.predict(self.mf.IFMR.wd_m_max)
+        self.mBH_min = self.mf.mBH_min
+
+        # Here are a bunch of masks for the bins
+        self.MS_mask = np.ones_like(self.mj, dtype=bool)
+        self.MS_mask[self.nms + 1 :] = False
+        self.WD_mask = (self.mj <= self.mWD_max) & ~self.MS_mask
+        self.NS_mask = (self.mj < self.mBH_min) & (self.mj > self.mWD_max)
+        self.BH_mask = self.mj >= self.mBH_min
+
+    def dump(self):
+        """
+        Dump current config of `BinaryShift`.
+        """
+        # Mass bins
+        print(f"{self.mj = }")
+
+        print(f"{self.Mj = }")
+
+        # These ones might be set
+        try:
+            print(f"{self.q = }")
+        except:
+            pass
+
+        try:
+            print(f"{self.fb = }")
+        except:
+            pass
+
+        # IFMR Stuff
+        print(f"{self.nms = }")
+
+        print(f"{self.mWD_max = }")
+
+        print(f"{self.mBH_min = }")
+
+    def shift_equal(self, fb):
+        """
+        Shift mass to create binaries of equal mass, amount of mass shifted is determined by `fb`.
+        """
+
+        if fb > 1.0 or fb < 0:
+            raise ValueError("fb must be between 0 and 1.")
+
+        self.q = 1
+        self.fb = fb
+
+        # don't mess with original
+        mj = self.mj.copy()
+        Mj = self.Mj.copy()
+
+        # loop through the MS mass bins
+        for i in range(self.nms):
+
+            print()
+            print(f"current mj: {mj[i]:.3f}")
+
+            # get mass of companion
+            companion_mass = mj[i] * self.q
+            print(f"{companion_mass = :.3f}")
+
+            # mass of new bin
+            mj_bin = mj[i] + companion_mass
+            print(f"new mass: {mj_bin:.3f} ")
+
+            # if the companion is smaller than the lightest MS bin, just skip it
+            if companion_mass < np.min(mj):
+                print(
+                    f"companion mass {companion_mass:.3f} smaller than {np.min(mj):.3f}, skipping"
+                )
+                pass
+            else:
+
+                # total mass in binaries for this new bin
+                Mj_bin = Mj[i] * self.fb
+                print(f"total mass in binaries {Mj_bin:.3f}")
+
+                # add in the new mean mass bin
+                mj = np.append(mj, mj_bin)
+
+                # add in the new total mass bin
+                Mj = np.append(Mj, Mj_bin)
+
+                # remove the mass from the old total mass bin
+                Mj[i] -= Mj_bin
+        return mj, Mj
+
+    def shift_q(self, fb, q):
+        """
+        Shift mass in to binaries with mass function `q`, amount of mass shifted determined by `fb`.
+        (TODO) Eventually this will support lists of `fb` and `q`.
+        """
+
+        if q > 1.0 or q < 0:
+            raise ValueError("q must be between 0 and 1.")
+
+        if fb > 1.0 or fb < 0:
+            raise ValueError("fb must be between 0 and 1.")
+
+        self.fb = fb
+        self.q = q
+
+        # don't mess with original
+        mj = self.mj.copy()
+        Mj = self.Mj.copy()
+
+        # loop through the MS mass bins
+        for i in range(self.nms):
+            print()
+            print(f"current mj: {mj[i]:.3f}")
+
+            # get mass of companion
+            companion_mass = mj[i] * self.q
+            print(f"{companion_mass = :.3f}")
+
+            # if the companion is smaller than the lightest MS bin, just skip it
+            if companion_mass < np.min(mj):
+                print(
+                    f"companion mass {companion_mass:.3f} smaller than {np.min(mj):.3f}, skipping"
+                )
+                pass
+            else:
+
+                # find closest bin to companion mass
+                companion_idx = np.argmin(np.abs(mj[: self.nms] - companion_mass))
+                print(f"closest {companion_idx = }")
+                # here change the mass of the companion to the mass of the closest bin (TODO: do we want this?)
+                companion_mass = mj[companion_idx]
+                print(f"closest {companion_mass = :.3f}")
+
+                # mass of new bin
+                binary_mj = mj[i] + companion_mass
+                print(f"new mass: {binary_mj:.3f} ")
+
+                # get total mass of new binary bin, will be (fb * mj) + (fb * companion mass bin)
+                primary_Mj = self.fb * Mj[i]
+                companion_Mj = self.fb * Mj[companion_idx]
+
+                binary_Mj = primary_Mj + companion_Mj
+
+                # add in new binary mean mass bin
+                mj = np.append(mj, binary_mj)
+
+                # add total mass to new binary bin
+                Mj = np.append(Mj, binary_Mj)
+
+                # remove mass from both primary, companion bins
+                Mj[i] -= primary_Mj
+                Mj[companion_idx] -= companion_Mj
+
+        return mj, Mj
+
+    def shift_kroupa():
+        """
+        (TODO) Shift mass according to `fb` and `q` determined by random draw from Kroupa IMF.
+        """
+        pass
+
+    def shift_solar():
+        """
+        (TODO) Shift mass according to `fb` and `q` in the solar neighborhood.
+        """
+        pass
---
+++ tests/test_binaryshift.py
@@ -0,0 +1,77 @@
+from ssptools import evolve_mf_3 as emf3
+import numpy as np
+import pytest
+
+from binaryshift import BinaryShift
+
+
+# Setup:
+
+m123 = [0.1, 0.5, 1.0, 100]  # Slope breakpoints for initial mass function
+a12 = [-0.468, -1.178, -2.117]  # Slopes for initial mass function
+nbin12 = [5, 5, 20]
+
+# Output times for the evolution
+tout = np.array([11000])
+
+# Integration settings
+N0 = 5e5  # Normalization of stars
+Ndot = -0.0001  # Regulates how low mass objects are depleted default -20, 0 for 47 Tuc
+tcc = 0  # Core collapse time
+NS_ret = 0.1  # Initial neutron star retention
+BH_ret_int = 1  # Initial Black Hole retention
+BH_ret_dyn = 0.00235  # Dynamical Black Hole retention
+FeHe = -0.7  # Metallicity
+
+
+@pytest.fixture
+# TODO: figure out how to ignore the SSPTools warnings
+def f():
+    # ignore the warnings from SSPTools
+    f = emf3.evolve_mf(
+        m123=m123,
+        a12=a12,
+        nbin12=nbin12,
+        tout=tout,
+        N0=N0,
+        Ndot=Ndot,
+        tcc=tcc,
+        NS_ret=NS_ret,
+        BH_ret_int=BH_ret_int,
+        BH_ret_dyn=BH_ret_dyn,
+        FeHe=FeHe,
+        natal_kicks=True,
+        vesc=100,
+    )
+    return f
+
+
+@pytest.fixture
+def mj_Mj(f):
+    cs = f.Ns[-1] > 10 * f.Nmin
+    cr = f.Nr[-1] > 10 * f.Nmin
+    mj = np.r_[f.ms[-1][cs], f.mr[-1][cr]]
+    Mj = np.r_[f.Ms[-1][cs], f.Mr[-1][cr]]
+    dms = f.mes[-1][1:] - f.mes[-1][0:-1]
+    nms = len(f.ms[-1][cs])
+    return mj, Mj
+
+
+@pytest.fixture
+def binshift(mj_Mj, f):
+    mj, Mj = mj_Mj
+    bs = BinaryShift(mj=mj, Mj=Mj, MF=f)
+    return bs
+
+
+# test mass conservation
+def test_shift_q(mj_Mj, binshift):
+    mj, Mj = mj_Mj
+    mj_new, Mj_new = binshift.shift_q(fb=0.3, q=0.7)
+    assert np.isclose(np.sum(Mj), np.sum(Mj_new))
+
+
+def test_shift_equal(mj_Mj, binshift):
+    mj, Mj = mj_Mj
+    mj_new, Mj_new = binshift.shift_equal(fb=0.3)
+    assert np.isclose(np.sum(Mj), np.sum(Mj_new))
