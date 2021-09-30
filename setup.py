a440540f234701be514af7d696abc3fa64280581
Peter Smith <smith.peter.902@gmail.com>
2021-09-30 13:28:16 -0300

    add package info

--- setup.py
+++ setup.py
@@ -4,4 +4,12 @@ setup( # pragma: no cover
     name="binaryshift",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    author = "Peter Smith",
+    email = "smith.peter.902@gmail.com",
+    uri = "https://github.com/pjs902/honors-thesis",
+    license = "MIT",
+    description = "Shift mass bins to emulate binary stars",
+    copyright = "Copyright 2021 Peter Smith",
+    python_requires=">=3.8",
+    version = "0.1.0",
 )
