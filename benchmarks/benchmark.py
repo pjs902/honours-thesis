e27be7d3e7658a4fb9f0948ebf6fa3585f864f59
Peter Smith <smith.peter.902@gmail.com>
2021-12-02 16:38:30 -0400

    start on some benchmarking

---
+++ benchmarks/benchmark.py
@@ -0,0 +1,55 @@
+from timeit import timeit
+from ssptools import evolve_mf_3 as emf3
+from limepy import limepy
+
+import numpy as np
+import scipy as sp
+
+from binaryshift import BinaryShift
+
+
+# setup SSPTools:
+print("Setting up SSPTools... bins = [5,5,20] (default)")
+m123 = [0.1, 0.5, 1.0, 100]  # Slope breakpoints for initial mass function
+a12 = [-0.468, -1.178, -2.117]  # Slopes for initial mass function
+a12 = [-0.3, -1.3, -2.3]  # Slopes for initial mass function
+# up the number of low mass bins from 5 to 15, fixes the mass ratio distribution and doens't have a have impact on runtime
+nbin12 = [5, 5, 20]
+# Output times for the evolution
+tout = np.array([11000])
+# Integration settings
+N0 = 5e6  # Normalization of stars
+Ndot = 0.0  # -0.0001  # Regulates how low mass objects are depleted default -20, 0 for 47 Tuc
+tcc = 0  # Core collapse time
+NS_ret = 0.1  # Initial neutron star retention
+BH_ret_int = 1  # Initial Black Hole retention
+BH_ret_dyn = 0.00235  # Dynamical Black Hole retention
+FeHe = -0.7  # Metallicity
+f = emf3.evolve_mf(
+    m123=m123,
+    a12=a12,
+    nbin12=nbin12,
+    tout=tout,
+    N0=N0,
+    Ndot=Ndot,
+    tcc=tcc,
+    NS_ret=NS_ret,
+    BH_ret_int=BH_ret_int,
+    BH_ret_dyn=BH_ret_dyn,
+    FeHe=FeHe,
+    natal_kicks=True,
+    vesc=100,
+)
+cs = f.Ns[-1] > 10 * f.Nmin
+cr = f.Nr[-1] > 10 * f.Nmin
+mj = np.r_[f.ms[-1][cs], f.mr[-1][cr]]
+Mj = np.r_[f.Ms[-1][cs], f.Mr[-1][cr]]
+dms = f.mes[-1][1:] - f.mes[-1][0:-1]
+nms = len(f.ms[-1][cs])
+print("Running limepy models...")
+time = timeit(
+    "limepy(M=1e6, phi0=6, g=0.8, mj=mj, Mj=Mj, delta=0.45, rh=8, ra=10 ** 1.23, project=True)",
+    globals=globals(),
+    number=1,
+)
+print(f"Time: {time:.4f} seconds")
