8697f4e0490b4f2f8cfcad55b8f2e5cb3ba3af0c
Peter Smith <smith.peter.902@gmail.com>
2021-12-03 08:35:11 -0400

    first basic benchmarks

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -8,6 +8,9 @@ import scipy as sp
 from binaryshift import BinaryShift
 
 
+RUNS = 5
+
+
 # setup SSPTools:
 print("Setting up SSPTools... bins = [5,5,20] (default)")
 m123 = [0.1, 0.5, 1.0, 100]  # Slope breakpoints for initial mass function
@@ -50,6 +53,101 @@ print("Running limepy models...")
 time = timeit(
     "limepy(M=1e6, phi0=6, g=0.8, mj=mj, Mj=Mj, delta=0.45, rh=8, ra=10 ** 1.23, project=True)",
     globals=globals(),
-    number=1,
+    number=RUNS,
 )
-print(f"Time: {time:.4f} seconds")
+print(f"Time: {time/RUNS:.4f} seconds")
+
+
+print("Setting up SSPTools... bins = [15,5,20] (higher-res in low mass)")
+nbin12 = [15, 5, 20]
+f = emf3.evolve_mf(
+    m123=m123,
+    a12=a12,
+    nbin12=nbin12,
+    tout=tout,
+    N0=N0,
+    Ndot=Ndot,
+    tcc=tcc,
+    NS_ret=NS_ret,
+    BH_ret_int=BH_ret_int,
+    BH_ret_dyn=BH_ret_dyn,
+    FeHe=FeHe,
+    natal_kicks=True,
+    vesc=100,
+)
+cs = f.Ns[-1] > 10 * f.Nmin
+cr = f.Nr[-1] > 10 * f.Nmin
+mj = np.r_[f.ms[-1][cs], f.mr[-1][cr]]
+Mj = np.r_[f.Ms[-1][cs], f.Mr[-1][cr]]
+dms = f.mes[-1][1:] - f.mes[-1][0:-1]
+nms = len(f.ms[-1][cs])
+print("Running limepy models...")
+time = timeit(
+    "limepy(M=1e6, phi0=6, g=0.8, mj=mj, Mj=Mj, delta=0.45, rh=8, ra=10 ** 1.23, project=True)",
+    globals=globals(),
+    number=RUNS,
+)
+print(f"Time: {time/RUNS:.4f} seconds")
+
+
+print("Setting up SSPTools... bins = [25,5,20] (10 bins added to high-res)")
+nbin12 = [25, 5, 20]
+f = emf3.evolve_mf(
+    m123=m123,
+    a12=a12,
+    nbin12=nbin12,
+    tout=tout,
+    N0=N0,
+    Ndot=Ndot,
+    tcc=tcc,
+    NS_ret=NS_ret,
+    BH_ret_int=BH_ret_int,
+    BH_ret_dyn=BH_ret_dyn,
+    FeHe=FeHe,
+    natal_kicks=True,
+    vesc=100,
+)
+cs = f.Ns[-1] > 10 * f.Nmin
+cr = f.Nr[-1] > 10 * f.Nmin
+mj = np.r_[f.ms[-1][cs], f.mr[-1][cr]]
+Mj = np.r_[f.Ms[-1][cs], f.Mr[-1][cr]]
+dms = f.mes[-1][1:] - f.mes[-1][0:-1]
+nms = len(f.ms[-1][cs])
+print("Running limepy models...")
+time = timeit(
+    "limepy(M=1e6, phi0=6, g=0.8, mj=mj, Mj=Mj, delta=0.45, rh=8, ra=10 ** 1.23, project=True)",
+    globals=globals(),
+    number=RUNS,
+)
+print(f"Time: {time/RUNS:.4f} seconds")
+
+print("Setting up SSPTools... bins = [25,10,20] (15 bins added to high-res)")
+nbin12 = [25, 10, 20]
+f = emf3.evolve_mf(
+    m123=m123,
+    a12=a12,
+    nbin12=nbin12,
+    tout=tout,
+    N0=N0,
+    Ndot=Ndot,
+    tcc=tcc,
+    NS_ret=NS_ret,
+    BH_ret_int=BH_ret_int,
+    BH_ret_dyn=BH_ret_dyn,
+    FeHe=FeHe,
+    natal_kicks=True,
+    vesc=100,
+)
+cs = f.Ns[-1] > 10 * f.Nmin
+cr = f.Nr[-1] > 10 * f.Nmin
+mj = np.r_[f.ms[-1][cs], f.mr[-1][cr]]
+Mj = np.r_[f.Ms[-1][cs], f.Mr[-1][cr]]
+dms = f.mes[-1][1:] - f.mes[-1][0:-1]
+nms = len(f.ms[-1][cs])
+print("Running limepy models...")
+time = timeit(
+    "limepy(M=1e6, phi0=6, g=0.8, mj=mj, Mj=Mj, delta=0.45, rh=8, ra=10 ** 1.23, project=True)",
+    globals=globals(),
+    number=RUNS,
+)
+print(f"Time: {time/RUNS:.4f} seconds")
